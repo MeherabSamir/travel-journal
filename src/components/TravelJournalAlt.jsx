ed9e0dba0822e5ea3c885d78eaa0b941999e58e3
MeherabSamir <meherabsamir.me@gmail.com>
2022-02-25 03:29:37 +0600

    Initital Commit 🚀

---
+++ src/components/TravelJournalAlt.jsx
@@ -0,0 +1,77 @@
+import datalist from "../data/datalist";
+import world from "../img/world.png";
+import locationIcon from "../img/location.png";
+
+function Header() {
+  return (
+    <header>
+      <img src={world} alt="world-icon" />
+      <h4>my travel journal.</h4>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      A React Solo Project From Scrimba Coded By{" "}
+      <a
+        href="https://github.com/MeherabSamir"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Meherab Samir
+      </a>
+    </footer>
+  );
+}
+
+function TravelJournalAlt() {
+  const journal = datalist.map(
+    ({
+      imageUrl,
+      location,
+      googleMapsUrl,
+      title,
+      startDate,
+      endDate,
+      description,
+    }) => {
+      return (
+        <div className="journal">
+          <div className="journal-img">
+            <img src={imageUrl} alt="img" />
+          </div>
+          <div className="journal-content">
+            <h5 className="journal-subtitle">
+              <span className="journal-location">
+                <img src={locationIcon} alt="icon" />
+                {location}
+              </span>
+              <span className="journal-map-link">
+                <a href={googleMapsUrl} target="_blank" rel="noreferrer">
+                  View on Google Maps
+                </a>
+              </span>
+            </h5>
+            <h2 className="journal-title">{title}</h2>
+            <h6 className="jounal-date">
+              <span className="jounal-date-start">{startDate}</span> -{" "}
+              <span className="jounal-date-end">{endDate}</span>
+            </h6>
+            <p className="journal-desc">{description}</p>
+          </div>
+        </div>
+      );
+    }
+  );
+  return (
+    <div className="travel-journal">
+      <Header />
+      <div className="journal-container">{journal}</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default TravelJournalAlt;
